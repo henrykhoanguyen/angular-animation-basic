36ba06692de235789b927b28fdd4caf5cd471f06
Khoa Nguyen <henrykhoanguyen@gmail.com>
2020-08-11 21:27:39 -0700

    added font awesome stuff into local lib

--- angular-animation-basic-client/src/app/app.module.ts
+++ angular-animation-basic-client/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
@@ -16,6 +18,7 @@ import { MatToolbarModule } from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FontAwesomeModule,
     MatToolbarModule
   ],
   providers: [],
