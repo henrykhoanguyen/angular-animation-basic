3bdb9c8a64a6d3d575f36f78dda01a65c0c70f11
Khoa Nguyen <henrykhoanguyen@gmail.com>
2020-08-18 00:22:21 -0700

    change css to scss

--- angular-animation-basic-client/src/app/pages/landing-page/landing-page.component.ts
+++ angular-animation-basic-client/src/app/pages/landing-page/landing-page.component.ts
@@ -1,39 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { JQueryStyleEventEmitter } from 'rxjs/internal/observable/fromEvent';
-declare var $:any;
+import { TimelineMax, Back } from 'gsap';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  items = [];
 
-  constructor() { }
-
-  ngOnInit() {
-  //   $(document).ready(function() {
-  //     $('.title').lettering();
-  //     $('.button').lettering();
-  //   });
-
-  //   $(document).ready(function() {
-  //     animation();
-  //   }, 1000);
-
-  //   $('.button').click(function() {
-  //     animation();
-  //   });
-
-
-  //   function animation() {
-  //     var title1 = new TimelineMax();
-  //     title1.to('.button', 0, {visibility: 'hidden', opacity: 0})
-  //     title1.staggerFromTo('.title span', 0.5,
-  //     {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
-  //     {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
-  //     title1.to('.button', 0.2, {visibility: 'visible' ,opacity: 1})
-  //   }
+  constructor() {
+    this.items = ['Bananas', 'Apple', 'Milk'];
   }
 
+  ngOnInit() {}
+
+  pushItem() {
+    this.items.push('Eggs');
+  }
+
+  removeItem() {
+    this.items.pop();
+  }
 }
--- angular-animation-basic-client/src/app/pages/to-do-list/to-do-list.component.ts
+++ angular-animation-basic-client/src/app/pages/to-do-list/to-do-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
-  styleUrls: ['./to-do-list.component.css']
+  styleUrls: ['./to-do-list.component.scss']
 })
 export class ToDoListComponent implements OnInit {
 
